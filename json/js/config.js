23cd401f4a33fa007b47f4c41b890c14ad4bae31
Uzair Mehmood <hey@illicitr.us>
2016-02-29 00:01:54 +0500

    Update config.js
    Setting to turn pagination on/off and MOAR COMMENTS

--- json/js/config.js
+++ json/js/config.js
@@ -5,16 +5,22 @@ $(function() {
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
 		// name of JSON file
-		JSONFile : "products.json"
+		JSONFile : "products.json",
+		// set to true if you want pages; false if you want infinite scroll
+		paginate : true
 	});
 });
+//Pagination function
 function paginate(){
 	setTimeout(function(){
 		$("div.holder").jPages({
-	    containerID : "simpleStore_container",
-	    perPage : 3,
-	    previous : false,
-	    next : false
+		    containerID : "simpleStore_container",
+		    // change this to how much rows of products you want per page. setting this to n means you get n*numColumns(above) products per page.
+		    perPage : 3,
+		    // change these if you want previous and next buttons in pagination controls.
+		    previous : false,
+		    next : false
+		    // for more pagination settings see http://luis-almeida.github.io/jPages/
 		});
 	}, 500);    
 };
